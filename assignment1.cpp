88a3a93b7cc1d282d4e650a39ed81f5326fe5fe7
vijay2411 <45455078+vijay2411@users.noreply.github.com>
2020-01-16 23:43:23 +0530

    Working OpenMP

--- assignment1.cpp
+++ assignment1.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std ;
 
 void print(double** matrix, int n){
@@ -28,12 +29,12 @@ double verify(int n,double **a, double** l, double** u, vector<int> pvector){
 		p[i][pvector[i]] = 1.0;
 	}
 	
-	for(int i = 0; i < n; i++){
-	   for(int j = 0; j < n; j++){
-		  // cout << p[i][j] << "  ";
-	   }
-	   cout<< endl;
-   }
+	// for(int i = 0; i < n; i++){
+	   // for(int j = 0; j < n; j++){
+		  // // cout << p[i][j] << "  ";
+	   // }
+	   // cout<< endl;
+   // }
    
 	double tmp = 0.0;
 	for(int i = 0; i < n; i++){
@@ -44,9 +45,9 @@ double verify(int n,double **a, double** l, double** u, vector<int> pvector){
 				//cout<<p[i][k]*a[k][j] - l[i][k]*u[k][j]<<" " ; 
 			}
 			result[i][j] = tmp;
-			cout<<tmp<<" " ;
+			// cout<<tmp<<" " ;
 		}
-		cout<<endl ;
+		// cout<<endl ;
 	}
 	double norm=0.0,colnorm = 0.0;
 	for(int i = 0; i < n; i++){
@@ -75,6 +76,9 @@ int main(int argc, char *argv[]) {
 	u = new double* [n];
 	double **l;
 	l = new double* [n];
+
+	auto start = std::chrono::high_resolution_clock::now();	
+#pragma omp parallel for	
 	for (int i=0; i < n; i++){
 		a[i] = new double[n];
 		u[i] = new double[n];
@@ -86,85 +90,112 @@ int main(int argc, char *argv[]) {
 	vector<int> p(n,0);
 	double drand48();
 	double max;
+
 	for(int i =0; i < n; i++){
-	   p[i] = i;
-	   for(int j = 0; j < n; j++){
-		   if( j < i ){
-			   u[i][j] = 0.0;
-			   l[i][j] = drand48();
-			   }
-		   else if(j == i){
-			   l[i][j] = 1.0;
-			   u[i][j] = drand48();
-			   }
-		   else{
-			   u[i][j] = drand48();
-			   l[i][j] = 0.0;
-			   }
-		   a[i][j] = drand48();
-		   a_orig[i][j] = a[i][j];		   
-	   }  
-   }
+		p[i] = i;		
+		for(int j = 0; j < n; j++){
+			if( j < i ){
+				u[i][j] = 0.0;
+				l[i][j] = drand48();
+			}
+			else if(j == i){
+				l[i][j] = 1.0;
+				u[i][j] = drand48();
+			}
+			else{
+				u[i][j] = drand48();
+				l[i][j] = 0.0;
+			}
+			a[i][j] = drand48();
+			a_orig[i][j] = a[i][j];		   
+		}  
+	}
    
    
-   for(int k = 0; k < n; k++){
-	   max = 0.0;
-	   //print(a,n);
-	   for(int i = k; i < n; i++){
-		   cout<<"fabs: "<<fabs(a[i][k]) ;
-		   if(fabs(a[i][k])> max){
-			   max = fabs(a[i][k]);
-			   kdash = i;
-		   }
-		   cout<<"MAX "<<max<<endl ;
-	   }
-	   cout<<k<<" "<<kdash<<endl;
-	   
-	   //cout<<endl ;
-	   if(max==0.0){
-		   cerr<<"Singular matrix."<<endl;
-	   }
-	   int temp = p[k];
-	   p[k] = p[kdash];
-	   p[kdash] = temp;
-	   //print(a,n);
-	   double* tmp = a[k];
-	   a[k] = a[kdash];
-	   a[kdash] = tmp;
-	   //print(a,n);
-	   
-	   double tmpdbl=0.0;
-	   
-	   for(int i = 0; i < k; i++){
-		   tmpdbl = l[k][i];
-		   l[k][i] = l[kdash][i];
-		   l[kdash][i] = tmpdbl;
-	   }
-	   //print(a,n);
-	   u[k][k] = a[k][k];
-	   for(int i = k+1; i < n; i++){
+	for(int k = 0; k < n; k++){
+		max = 0.0;
+			
+
+		for(int i = k; i < n; i++){
+			if(fabs(a[i][k])> max){
+				max = fabs(a[i][k]);
+				kdash = i;
+			}
+		}
+		
+		if(max==0.0){
+			cerr<<"Singular matrix."<<endl;
+		}
+		
+		//swap in p 
+		int temp = p[k];
+		p[k] = p[kdash];
+		p[kdash] = temp;
+		
+		//swap rows in A
+		double* tmp = a[k];
+		a[k] = a[kdash];
+		a[kdash] = tmp;
+		   
+		double tmpdbl=0.0;
+
+#pragma omp parallel for private(tmpdbl) if(k>500)
+		for(int i = 0; i < k; i++){
+			tmpdbl = l[k][i];
+			l[k][i] = l[kdash][i];
+			l[kdash][i] = tmpdbl;
+		}
+
+		u[k][k] = a[k][k];
+		
+#pragma omp parallel for 
+		for(int i = k+1; i < n; i++){
 		   l[i][k] = a[i][k]*1.0/u[k][k];
 		   u[k][i] = a[k][i];
+		}
+		
+	int j=0;
+	double val=0.0 ;
+	double utemp[n-k-1] ;
+	
+#pragma omp parallel for
+	for(int i=0;i<n-k-1;i++){
+		utemp[i]=u[k][i+k+1] ;
+	}
+
+double *tempdub ;
+#pragma omp parallel for private(j,val,tempdub)
+	for(int i = k+1; i < n; i++){
+		val=l[i][k] ;
+		tempdub=a[i] ;
+	   for(j = k+1; j<n; j++){
+		   tempdub[j] -= val*utemp[j-k-1];	
 	   }
-	   
-	   for(int i = k+1; i < n; i++){
-		   for(int j = k+1; j < n; j++){
-			   a[i][j] -= l[i][k]*u[k][j];
-		   }
-	   }
-   }
-   print(a,n);
-   print(l,n);
-   print(u,n);
+	}
+		// int i=0,j=0 ;
+		
+	// #pragma omp parallel for private(i,j)
+		// for(int itr =0;itr<(n-k-1)*(n-k-1);itr++){
+			// i=itr/(n-k-1) +k+1;
+			// j=itr%(n-k-1) + k+1;
+			// a[i][j] -= l[i][k]*u[k][j];
+		// }
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+	cout<<"Time Taken: "<<chrono::duration_cast<chrono::microseconds>(end-start).count()/1000000.0<<endl ;
+   //print(a,n);
+   //print(l,n);
+   //print(u,n);
    double result = verify(n,a_orig,l,u,p);
    cout<<result<<endl;
    
-   for(int i = 0; i < n; i++){
-	   for(int j = 0; j < n; j++){
-		  // cout << u[i][j] << "  ";
-	   }
-	   cout<< endl;
-   }
+   // for(int i = 0; i < n; i++){
+	   // for(int j = 0; j < n; j++){
+		  // // cout << u[i][j] << "  ";
+	   // }
+	   // cout<< endl;
+   // }
    
    return 0;
 }
