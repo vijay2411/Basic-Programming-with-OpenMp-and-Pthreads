9c68834fc833f864c21bb42f4b39dca7bbed7020
vijay2411 <45455078+vijay2411@users.noreply.github.com>
2020-01-22 01:11:07 +0530

    Real Pthread code

--- assignment1pthreads.cpp
+++ assignment1pthreads.cpp
@@ -1,62 +1,105 @@
 #include <bits/stdc++.h>
-#include <omp.h>
+#include <pthread.h>
 using namespace std ;
 
-// void print(double** matrix, int n){
-	// for(int i = 0; i < n; i++){
-	   // for(int j = 0; j < n; j++){
-		   // cout << matrix[i][j] << "  ";
-	   // }
-	   // cout<< endl;
-   // }
-   // cout<<endl;
+double **a;
+double **a_orig;
+double **u;
+double **l;
+int n;
+int cores;
+
+void print(double** matrix, int n){
+	for(int i = 0; i < n; i++){
+	   for(int j = 0; j < n; j++){
+		   cout << matrix[i][j] << "  ";
+	   }
+	   cout<< endl;
+   }
+   cout<<endl;
 	
-// }
-// double verify(int n,double **a, double** l, double** u, vector<int> pvector){
-	// double p[n][n]; double result[n][n];
-	// for(int i =0 ; i < n; i++){
-		// for(int j = 0; j < n; j++){
-			// p[i][j] = 0.0;
-			// result[i][j] = 0.0;
-		// }
-	// }
-	// for(int i = 0; i < n; i++){
-		// p[i][pvector[i]] = 1.0;
-	// }
-	// double tmp = 0.0;
-	// for(int i = 0; i < n; i++){
-		// for(int j = 0; j < n; j++){
-			// tmp = 0.0;
-			// for(int k = 0; k < n; k++){
-				// tmp = tmp + p[i][k]*a[k][j] - l[i][k]*u[k][j]; 
-			// }
-			// result[i][j] = tmp;
-		// }
-	// }
-	// double norm=0.0,colnorm = 0.0;
-	// for(int i = 0; i < n; i++){
-		// colnorm = 0.0;
-		// for(int j = 0; j < n; j++){
-			// colnorm += result[i][j]*result[i][j];
-		// }
-		// norm += sqrt(colnorm);
-	// }
+}
+double verify(int n,double **a, double** l, double** u, vector<int> pvector){
+	double **p; double **result;
+	p = new double* [n]; result = new double* [n];
+	for (int i=0; i < n; i++){
+		p[i] = new double[n];
+		result[i] = new double[n];
+	}
+	for(int i =0 ; i < n; i++){
+		for(int j = 0; j < n; j++){
+			p[i][j] = 0.0;
+			result[i][j] = 0.0;
+		}
+	}
+	for(int i = 0; i < n; i++){
+		p[i][pvector[i]] = 1.0;
+	}
 	
-	// return norm;
+	double tmp = 0.0;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			tmp = 0.0;
+			for(int k = 0; k < n; k++){
+				tmp = tmp + p[i][k]*a[k][j] - l[i][k]*u[k][j]; 
+				//cout<<p[i][k]*a[k][j] - l[i][k]*u[k][j]<<" " ; 
+			}
+			result[i][j] = tmp;
+			// cout<<tmp<<" " ;
+		}
+		// cout<<endl ;
+	}
+	double norm=0.0,colnorm = 0.0;
+	for(int i = 0; i < n; i++){
+		colnorm = 0.0;
+		for(int j = 0; j < n; j++){
+			colnorm += result[i][j]*result[i][j];
+		}
+		norm += sqrt(colnorm);
+	}
 	
-// }
+	return norm;
+	
+}
+
+void* assignParallel(void* pair_id_k){
+	int id = (*(pair<int,int> *)pair_id_k).first ;
+	int k = (*(pair<int,int> *)pair_id_k).second ;
+	int total=n-1-k ;
+	int start = (k+1)+id*total/cores ;
+	int end = min((k+1)+(id+1)*total/cores,n) ;
+	for(int i=start;i<end;i++){		
+		for(int j=k+1;j<n;j++){
+			a[i][j] -= l[i][k]*u[k][j];
+		}
+	}
+}
 
 int main(int argc, char *argv[]) {
    // printf() displays the string inside quotation
-    string arg1 = argv[1] ;
-	int n = stoi(arg1);
+    string arg1= argv[1] ;
+    string arg2= argv[2] ;
+	cores= stoi(arg1);
+	n = stoi(arg2);
 	int kdash;
 	//printf("n = %d", n);
-	double a[n][n],a_orig[n][n], u[n][n], l[n][n] ;
+	a = new double* [n];
+	a_orig = new double* [n];
+	u = new double* [n];
+	l = new double* [n];
+
 	auto start = std::chrono::high_resolution_clock::now();	
+	for (int i=0; i < n; i++){
+		a[i] = new double[n];
+		u[i] = new double[n];
+		l[i] = new double[n];
+		a_orig[i] = new double[n];
+	}
+   
+   
 	vector<int> p(n,0);
 	double drand48();
-	double max=0.0;
+	double max;
 
 	for(int i =0; i < n; i++){
 		p[i] = i;
@@ -82,9 +125,7 @@ int main(int argc, char *argv[]) {
 for(int k = 0; k < n; k++){
 	max = 0.0;
 		
-#pragma omp parallel for
 	for(int i = k; i < n; i++){
-#pragma omp critical
 		if(fabs(a[i][k])> max){
 			max = fabs(a[i][k]);
 			kdash = i;
@@ -101,17 +142,12 @@ for(int k = 0; k < n; k++){
 	p[kdash] = temp;
 	
 	//swap rows in A
-	double tmp=0.0 ;
-#pragma omp parallel for private(tmp)
-	for(int i=0;i<n;i++){
-		tmp=a[k][i] ;
-		a[k][i]=a[kdash][i] ;
-		a[kdash][i]=tmp ;
-	}
-	
+	double* tmp = a[k];
+	a[k] = a[kdash];
+	a[kdash] = tmp;
+	   
 	double tmpdbl=0.0;
 
-#pragma omp parallel for private(tmpdbl)
 	for(int i = 0; i < k; i++){
 		tmpdbl = l[k][i];
 		l[k][i] = l[kdash][i];
@@ -120,42 +156,39 @@ for(int k = 0; k < n; k++){
 
 	u[k][k] = a[k][k];
 	
-#pragma omp parallel for
 	for(int i = k+1; i < n; i++){
 	   l[i][k] = a[i][k]*1.0/u[k][k];
 	   u[k][i] = a[k][i];
 	}
 
-	// for(int i = k+1; i < n; i++){
-	   // for(int j = k+1; j < n; j++){
-		   // a[i][j] -= l[i][k]*u[k][j];
-	   // }
-	// }
-	int i=0,j=0 ;
-	
-#pragma omp parallel for private(i,j)
-	for(int itr =0;itr<(n-k-1)*(n-k-1);itr++){
-		i=itr/(n-k-1) +k+1;
-		j=itr%(n-k-1) + k+1;
-		a[i][j] -= l[i][k]*u[k][j];
+	pthread_t thread[cores] ;
+	pair<int,int> arm[cores] ;
+	for(int i=0;i<cores;i++){
+		arm[i]= make_pair(i,k) ;
 	}
+	
+	for(int i=0;i<cores;i++){
+		pthread_create(&thread[i], NULL, &assignParallel, (void*)&arm[i]); 
+	}
+	for(int i=0;i<cores;i++)
+		pthread_join(thread[i],NULL) ;
+
 }
 
 	auto end = std::chrono::high_resolution_clock::now();
-cout<<"Time Taken: "<<chrono::duration_cast<chrono::microseconds>(end-start).count()/1000000.0<<endl ;
+	cout<<"Time Taken: "<<chrono::duration_cast<chrono::microseconds>(end-start).count()/1000000.0<<endl ;
    //print(a,n);
    //print(l,n);
    //print(u,n);
-//   double result = verify(n,a_orig,l,u,p);
-//   cout<<result<<endl;
-   
-   // for(int i = 0; i < n; i++){
-	   // for(int j = 0; j < n; j++){
-		  // // cout << u[i][j] << "  ";
-	   // }
-	   // cout<< endl;
-   // }
-   
+   //double result = verify(n,a_orig,l,u,p);
+   //cout<<result<<endl;
+
+	for (int i=0; i < n; i++){
+		delete a[i] ;
+		delete u[i] ;
+		delete l[i] ;
+		delete a_orig[i] ;
+	}
+	delete a,u,l,a_orig ;
    return 0;
 }
-
