d06afa18d73732692fe9bcac3bdb988ddea632c7
vijay2411 <45455078+vijay2411@users.noreply.github.com>
2020-01-24 14:23:44 +0530

    Serial Code with updated comments

--- Serial.cpp
+++ Serial.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <stdlib.h>
 #include <omp.h>
 using namespace std ;
 
@@ -12,13 +13,17 @@ void print(double** matrix, int n){
    cout<<endl;
 	
 }
+
 double verify(int n,double **a, double** l, double** u, vector<int> pvector){
+	
+	//defining p matrix, result matrix (=PA-LU)
 	double **p; double **result;
 	p = new double* [n]; result = new double* [n];
 	for (int i=0; i < n; i++){
 		p[i] = new double[n];
 		result[i] = new double[n];
 	}
+	//initialisation of p,result matrix
 	for(int i =0 ; i < n; i++){
 		for(int j = 0; j < n; j++){
 			p[i][j] = 0.0;
@@ -29,19 +34,19 @@ double verify(int n,double **a, double** l, double** u, vector<int> pvector){
 		p[i][pvector[i]] = 1.0;
 	}
 	
+	//PA-LU loop
 	double tmp = 0.0;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			tmp = 0.0;
 			for(int k = 0; k < n; k++){
 				tmp = tmp + p[i][k]*a[k][j] - l[i][k]*u[k][j]; 
-				//cout<<p[i][k]*a[k][j] - l[i][k]*u[k][j]<<" " ; 
 			}
 			result[i][j] = tmp;
-			// cout<<tmp<<" " ;
 		}
-		// cout<<endl ;
 	}
+	
+	//LU norm
 	double norm=0.0,colnorm = 0.0;
 	for(int i = 0; i < n; i++){
 		colnorm = 0.0;
@@ -56,11 +61,11 @@ double verify(int n,double **a, double** l, double** u, vector<int> pvector){
 }
 
 int main(int argc, char *argv[]) {
-   // printf() displays the string inside quotation
-    string arg1= argv[1] ;
+	//Dimension N of Matrix
+	string arg1 = argv[1] ;
 	int n = stoi(arg1);
-	int kdash;
-	//printf("n = %d", n);
+	
+	//Defined Matrix A, Saved Instance of A, Upper triangular matrix, Lower triangular matrix
 	double **a;
 	a = new double* [n];
 	double **a_orig;
@@ -70,7 +75,10 @@ int main(int argc, char *argv[]) {
 	double **l;
 	l = new double* [n];
 
+	//Time measurement, clock start.
 	auto start = std::chrono::high_resolution_clock::now();	
+	
+	//memory allocation to matrices
 	for (int i=0; i < n; i++){
 		a[i] = new double[n];
 		u[i] = new double[n];
@@ -78,13 +86,12 @@ int main(int argc, char *argv[]) {
 		a_orig[i] = new double[n];
 	}
    
-   
+	//the Pie Vector,random initialisation of matrices
 	vector<int> p(n,0);
 	double drand48();
-	double max;
 
 	for(int i =0; i < n; i++){
-		p[i] = i;
+		p[i] = i;		
 		for(int j = 0; j < n; j++){
 			if( j < i ){
 				u[i][j] = 0.0;
@@ -102,70 +109,87 @@ int main(int argc, char *argv[]) {
 			a_orig[i][j] = a[i][j];		   
 		}  
 	}
-   
-   
-for(int k = 0; k < n; k++){
-	max = 0.0;
+	
+	//used for finding max in an array
+	double maxelem;
+	int kdash;	
+	
+	//THE N Iterations
+	for(int k = 0; k < n; k++){
+		maxelem = 0.0;
+		auto startfor = std::chrono::high_resolution_clock::now();	
+	
+		for(int i = k; i < n; i++){
+			if(fabs(a[i][k])> maxelem){
+				maxelem = fabs(a[i][k]);
+				//index of max element
+				kdash = i;
+			}
+		}
 		
-	for(int i = k; i < n; i++){
-		if(fabs(a[i][k])> max){
-			max = fabs(a[i][k]);
-			kdash = i;
+		//Singular Matrix
+		if(maxelem==0.0){
+			cerr<<"Singular matrix."<<endl;
+		}
+		
+		//swapping p[k] & p[k'] 
+		int temp = p[k];
+		p[k] = p[kdash];
+		p[kdash] = temp;
+		
+		//swapping rows A[k] & A[k']
+		double* tmp = a[k];
+		a[k] = a[kdash];
+		a[kdash] = tmp;
+
+		//swapping rows l[k] & l[k']
+		double tmpdbl=0.0;
+		for(int i = 0; i < k; i++){
+			tmpdbl = l[k][i];
+			l[k][i] = l[kdash][i];
+			l[kdash][i] = tmpdbl;
+		}
+		
+		u[k][k] = a[k][k];
+		
+		//assigning l[][],u[][] updated values
+		for(int i = k+1; i < n; i++){
+		   l[i][k] = a[i][k]*1.0/u[k][k];
+		   u[k][i] = a[k][i];
+		}
+		
+		//storing u[k,j] in a matrix for j=k to n-1 (algorithmic improvement)
+		double utemp[n-k-1] ;
+		for(int i=0;i<n-k-1;i++){
+			utemp[i]=u[k][i+k+1] ;
+		}
+
+		//defined for algorithmic improvements
+		double *tempdub ;
+		double val ;
+		int j=0;
+	
+		//upating A[][]
+		for(int i = k+1; i < n; i++){
+			val=l[i][k] ;
+			//pointer to ith row of A matrix
+			tempdub=a[i] ;
+			for(j = k+1; j<n; j++){
+				//Algo: "a(i,j) = a(i,j) - l(i,k)*u(k,j)"
+				tempdub[j] -= val*utemp[j-k-1];	
+			}
 		}
 	}
 	
-	if(max==0.0){
-		cerr<<"Singular matrix."<<endl;
-	}
-	
-	//swap in p 
-	int temp = p[k];
-	p[k] = p[kdash];
-	p[kdash] = temp;
-	
-	//swap rows in A
-	double* tmp = a[k];
-	a[k] = a[kdash];
-	a[kdash] = tmp;
-	   
-	double tmpdbl=0.0;
-
-	for(int i = 0; i < k; i++){
-		tmpdbl = l[k][i];
-		l[k][i] = l[kdash][i];
-		l[kdash][i] = tmpdbl;
-	}
-
-	u[k][k] = a[k][k];
-	
-	for(int i = k+1; i < n; i++){
-	   l[i][k] = a[i][k]*1.0/u[k][k];
-	   u[k][i] = a[k][i];
-	}
-
-
-	
-	for(int i = k+1; i < n; i++){
-	   for(int j = k+1; j < n; j++){
-		   a[i][j] -= l[i][k]*u[k][j];
-	   }
-	}
-	int i=0,j=0 ;
-		
-	// for(int itr =0;itr<(n-k-1)*(n-k-1);itr++){
-		// i=itr/(n-k-1) +k+1;
-		// j=itr%(n-k-1) + k+1;
-		// a[i][j] -= l[i][k]*u[k][j];
-	// }
-}
-
+	//Time measurements
 	auto end = std::chrono::high_resolution_clock::now();
-cout<<"Time Taken: "<<chrono::duration_cast<chrono::microseconds>(end-start).count()/1000000.0<<endl ;
-   //print(a,n);
-   //print(l,n);
-   //print(u,n);
-//   double result = verify(n,a_orig,l,u,p);
-//   cout<<result<<endl;
-
+	cout<<"Time Taken: "<<chrono::duration_cast<chrono::microseconds>(end-start).count()/1000000.0<<endl ;
+	
+	//Following code( Verification part ) is commented because they consume too much time for n=8000
+	/*
+	double result = verify(n,a_orig,l,u,p);
+	cout<<result<<endl;
+	*/
+   
    return 0;
 }
